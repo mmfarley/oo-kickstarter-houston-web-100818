b5e3169a588d34ee4ba532fdd387f716e4850334
Megan Farley <megfarley4@gmail.com>
2018-10-11 13:44:34 -0500

    changes for pair programming lab

---
+++ lib/backer.rb
@@ -0,0 +1,21 @@
+require_relative './project.rb'
+require 'pry'
+
+class Backer
+
+    attr_accessor :name, :backed_projects
+
+    def initialize(name)
+        @name = name
+        @backed_projects = []
+    end
+
+    def back_project(project)
+        self.backed_projects << project
+        project.backers << self
+    end
+
+    
+
+    #binding.pry
+end
